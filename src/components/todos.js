b032438e50dad6ddfe4de2af26e1cc7fa80bc9b7
Andrea <andrearivas4cinfor@gmail.com>
2023-02-18 09:47:12 +0100

    Add delete todo function

--- src/components/todos.js
+++ src/components/todos.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Trash, PencilSquare } from 'react-bootstrap-icons';
 
 const TodoList = ({ todos, setTodos }) => {
-  // Update the title of the todo object in todo state.
   const handleInputChange = (e, id) => {
     const newTodos = todos.map((todo) => {
-      // Using map to create a new array of todos.
       if (todo.id === id) {
         return { ...todo, title: e.target.value };
-        // is id matches we return a new todo object with same
-        // id and a new title based on the todo object.
       }
       return todo;
     });
     setTodos(newTodos);
   };
+
+  const handleDelete = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <ul>
       {todos.map((todo) => (
         <li className="todo-list" key={todo.id}>
+          <input className="checkbox" type="checkbox" />
           <input
             type="text"
             value={todo.title}
             onChange={(e) => handleInputChange(e, todo.id)}
+            readOnly
           />
+          <button
+            type="button"
+            className="button-edit"
+            onClick={() => handleDelete(todo.id)}
+          >
+            <PencilSquare />
+          </button>
+          <button type="button" className="button-delete">
+            <Trash onClick={() => handleDelete(todo.id)} />
+          </button>
         </li>
       ))}
     </ul>
-
   );
 };
+
 TodoList.propTypes = {
   todos: PropTypes.arrayOf.isRequired,
   setTodos: PropTypes.func.isRequired,
